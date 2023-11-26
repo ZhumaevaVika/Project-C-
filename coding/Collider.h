c7ed21de71f789c2f0c8208ade5cf20ce2bd8da7
ZhumaevaVika <s25781256@gmail.com>
2023-11-26 20:56:03 +0300

    tutorial animations, movement, collisions

---
+++ coding/Collider.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+
+class Collider{
+public:
+    Collider(sf::RectangleShape& body);
+    ~Collider();
+
+    void Move(float dx, float dy) { body.move(dx, dy); }
+
+    bool CheckCollider(Collider other, float push);
+    sf::Vector2f GetPosition() { return body.getPosition(); }
+    sf::Vector2f GetHalfSize() { return body.getSize() / 2.0f; }
+
+private:
+    sf::RectangleShape& body;
+};
---
+++ coding/Player.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include "Animation.h"
+#include "Collider.h"
+
+class Player{
+public:
+    Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
+    ~Player();
+
+    void Update(float deltaTime);
+    void Draw(sf::RenderWindow& window);
+
+    sf::Vector2f GetPosition() { return body.getPosition(); }
+    Collider GetCollider() { return Collider(body); }
+
+private:
+    sf::RectangleShape body;
+    Animation animation;
+    unsigned int row;
+    float speed;
+    bool faceRight;
+};
